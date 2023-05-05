0e168d5c57d43ee83f69bb7aa2c4a9f0429525e1
Jan <120336452+janPiMargalef@users.noreply.github.com>
2023-05-05 21:30:49 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.caixerautomaticm03 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.caixerautomaticm03 to javafx.fxml;
-    exports com.mycompany.caixerautomaticm03;
-}
